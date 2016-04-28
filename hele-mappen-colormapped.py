626f90a7b9a6730888a51c36a788b848565faeef
Folkmann <theme1256@gmail.com>
2016-04-28 11:33:29 +0200

    Flyttede legenden

--- hele-mappen-colormapped.py
+++ hele-mappen-colormapped.py
@@ -1,6 +1,7 @@
 # Importerer alle de libs der skal bruges og maaske lidt mere
 from pandas import DataFrame, read_csv
 from matplotlib import cm
+from matplotlib.font_manager import FontProperties
 import matplotlib.pyplot as plt
 import pandas as pd
 import sys
@@ -17,6 +18,10 @@ print('Matplotlib version ' + matplotlib.__version__)
 # Funktion, som kan generere en vaerdi mellem 0 og 255 til random farver
 r = lambda: random.randint(0,255)
 cmap = cm.get_cmap('jet')
+
+# Saetter stoerrelsen paa teksten ned
+fontP = FontProperties()
+fontP.set_size('small')
 
 # Finder alle filerne og laver en figur af hver enkelt, som gemmes som billede
 path = './'
@@ -43,5 +48,8 @@ frame = pd.concat(list_)
 ax = frame.plot(cmap=cmap)
 ax.set_xlabel('Sample')
 ax.set_ylabel('Latency [ms]')
+box = ax.get_position()
+ax.set_position([box.x0, box.y0+box.height*0.25, box.width, box.height*0.75])
+ax.legend(bbox_to_anchor=(0.5, -0.05), loc='upper center', ncol=5, fancybox=True, shadow=True, prop=fontP)
 fig = ax.get_figure()
 fig.savefig('foo.png')
